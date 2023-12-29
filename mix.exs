a644ffe817597678492fff65063a01fec8b5b3d9
c4710n <c4710n@users.noreply.github.com>
2023-12-29 20:17:54 +0800

    release: bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule FsBuild.MixProject do
   use Mix.Project
 
-  @version "1.0.3"
+  @version "1.1.0"
   @name "FsBuild"
   @description "A filesystem-based build engine which provides a flexible mechanism for parsing and processing files."
   @source_url "https://github.com/cozy-elixir/fs_build"
